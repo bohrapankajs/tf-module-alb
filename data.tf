36a2a9bf3879eb7fa98fcba1d166631debaff0b3
Pankajbohra <bohrapankajs@gmail.com>
2023-09-01 16:53:51 +0530

    test

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b51-tf-remote-state-bucket"
+    bucket = "mysecond-bucket-01"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
